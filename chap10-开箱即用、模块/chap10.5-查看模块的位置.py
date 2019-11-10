5ea53869c5639882695a5ee6978dfe5e21c9605f
LiangLiang <1073072541@qq.com>
2019-11-10 19:36:02 +0800

    pprint.pprint(sys.path) 查看模块位置

---
+++ chap10-开箱即用、模块/chap10.5-查看模块的位置.py
@@ -0,0 +1,19 @@
+"""
+查看模块的位置
+
+>>> import sys, pprint
+>>> pprint.pprint(sys.path)
+['',
+ 'C:\\Users\\heliang\\AppData\\Local\\Programs\\Python\\Python38-32\\Lib\\idlelib',
+ 'C:\\Users\\heliang\\AppData\\Local\\Programs\\Python\\Python38-32\\python38.zip',
+ 'C:\\Users\\heliang\\AppData\\Local\\Programs\\Python\\Python38-32\\DLLs',
+ 'C:\\Users\\heliang\\AppData\\Local\\Programs\\Python\\Python38-32\\lib',
+ 'C:\\Users\\heliang\\AppData\\Local\\Programs\\Python\\Python38-32',
+ 'C:\\Users\\heliang\\AppData\\Local\\Programs\\Python\\Python38-32\\lib\\site-packages']
+
+"""
+#
+# C:\Users\heliang\AppData\Local\Programs\Python\Python38-32\Lib\site-packages
+
+print("----- pprint.pprint(str) 一个非常强大的打印函数，需要导入 pprint 模块 ------")
+
