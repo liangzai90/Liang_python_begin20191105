df4be9718d9cd6c8bcba1427c84b41e40e792819
LiangLiang <1073072541@qq.com>
2019-11-10 19:18:42 +0800

    导入模块、重新导入模块

---
+++ chap10-开箱即用、模块/一个最简单的模块.py
@@ -0,0 +1,20 @@
+# chap10_1.py
+print("Hello, world! 20191110")
+
+
+"""
+# 导入模块chap10_1.py
+
+# 1.导入sys；2.导入python路径；4.导入自定义模块路径
+
+import sys
+sys.path.append("C:\\Users\\heliang\\AppData\\Local\\Programs\\Python\\Python38-32")
+sys.path.append("F:\\003_Project____Python\\pycharm_project\\PythonBegin_execise\\chap10")
+import chap10_1
+
+
+# 重新导入模块chap10_1.py
+import importlib
+chap10_1 = importlib.reload(chap10_1)
+
+"""
