0bf7f5dfd6c8e7f4a7c5fc60d8695970b60d73c2
LiangLiang <1073072541@qq.com>
2019-11-11 20:03:01 +0800

    线程化 服务器
    from socketserver import TCPServer, ThreadingMixIn, StreamRequestHandler

---
+++ chap14-网络编程/chap14.6-线程化 服务器.py
@@ -0,0 +1,13 @@
+print("-----线程化 服务器------")
+from socketserver import TCPServer, ThreadingMixIn, StreamRequestHandler
+
+class Server(ThreadingMixIn, TCPServer):pass
+
+class Handler(StreamRequestHandler):
+    def handle(self):
+        addr = self.request.getpeername()
+        print("Got connection from ", addr)
+        self.wfile.write(b"Thank you for connecting..")
+
+server = Server(("", 1234), Handler)
+server.serve_forever()
