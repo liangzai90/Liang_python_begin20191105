b9268291f5e774fc169519649af245a330c0902b
LiangLiang <1073072541@qq.com>
2019-11-13 21:00:30 +0800

    打包程序 setup.py

---
+++ chap18-程序打包/setup.py
@@ -0,0 +1,11 @@
+# file name: setup.py
+
+from setuptools import setup
+
+setup(
+    name='hello',
+    version='1.0',
+    description='A simple example',
+    author='He Liang Liang',
+    py_modules=['hello']
+)
