b21acf6ec2e64a9b5c10b4509ab7838828d7e3c1
LiangLiang <1073072541@qq.com>
2019-11-14 09:11:47 +0800

    flask web框架

--- chap15-Python&Web/chap15.9-使用web框架.py
+++ chap15-Python&Web/chap15.9-使用web框架.py
@@ -1,12 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('chap15.9.py')
-def powers(n=10):
-    return ', '.join(str(2**i) for i in range(n))
+@app.route('/')
+def hello():
+    return "hello Flask world!"
 
-# 找不到变量的处理方式
-# 将下面命令替换set FLASK_APP = chap15.9.py  成 $env:FLASK_APP = "chap15.9.py"
-# 卡壳了。。。环境配置不熟悉
+if __name__ == "__main__":
+    app.run()
+
+
 # RSS, XML-RPC 等等 高级内容不太好理解
 
