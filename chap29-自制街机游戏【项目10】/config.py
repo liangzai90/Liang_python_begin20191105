97dbf4e66cf551cd76917d8f791eb247f03a31d6
LiangLiang <1073072541@qq.com>
2019-11-17 01:38:42 +0800

    自制街机游戏
    还蛮有趣的

---
+++ chap29-自制街机游戏【项目10】/config.py
@@ -0,0 +1,28 @@
+# Configuration file for Squish
+# -----------------------------
+
+# Feel free to modify the configuration variables below to taste.
+# If the game is too fast or too slow, try to modify the speed
+# variables.
+
+# Change these to use other images in the game:
+banana_image = 'banana.png'
+weight_image = 'weight.png'
+splash_image = 'weight.png'
+
+# Change these to affect the general appearance:
+screen_size = 800, 600
+background_color = 255, 255, 255
+margin = 30
+full_screen = 0
+font_size = 48
+
+# These affect the behavior of the game:
+drop_speed = 1
+banana_speed = 20
+speed_increase = 1
+weights_per_level = 5
+banana_pad_top = 40
+banana_pad_side = 20
+# 数值越大，下降越慢
+speed_control = 5
