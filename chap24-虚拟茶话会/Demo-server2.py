7a472f13340040776d739e7d6f4edec1ef2aa39e
LiangLiang <1073072541@qq.com>
2019-11-16 11:39:12 +0800

    进一步扩展Demo
      conn.send("bye bye".encode('utf-8'))

---
+++ chap24-虚拟茶话会/Demo-server2.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8  -*-
+
+from asyncore import dispatcher
+import socket, asyncore
+
+class ChatServer(dispatcher):
+    def handle_accept(self):
+        conn, addr = self.accept()
+        print("Connection attemp from:", addr[0])
+        conn.send("bye bye".encode('utf-8'))
+
+s = ChatServer()
+s.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("", 5005))
+s.listen(5)
+asyncore.loop()
